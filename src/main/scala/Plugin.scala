db37b5f7b89265adb0353af083f868ef82b27866
Nick Adams <nick.adams@rakuten.com>
2015-04-08 07:49:06 -0600

    Adding the ability for plugins to tell if they can handle a message or not

--- src/main/scala/Plugin.scala
+++ src/main/scala/Plugin.scala
@@ -1,5 +1,6 @@
 package net.node3.scalabot
 
 trait Plugin {
-  def apply(from: String, to: String, message: String) : Option[String]
+  def apply(from: String, to: String, message: String): Option[String]
+  def handlesMessage(from: String, to: String, message: String): Boolean
 }
--- src/main/scala/plugins/Account.scala
+++ src/main/scala/plugins/Account.scala
@@ -4,5 +4,9 @@ import net.node3.scalabot.Plugin
 import net.node3.scalabot.db.DataCore
 
 class AccountPlugin extends Plugin with DataCore {
-  def apply(from: String, to: String, message: String) : Option[String] = Some("hello from account plugin")
+  def apply(from: String, to: String, message: String) : Option[String] = {
+    Some("hello from account plugin")
+  }
+
+  def handlesMessage(from: String, to: String, message: String) = true
 }
