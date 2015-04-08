4ad35c5fe55481869edbabb27109405bfef49580
Nick Adams <nick.adams@rakuten.com>
2015-04-08 11:35:03 -0600

    Refactoring the way the commands are executed in AccountPlugin

--- src/main/scala/plugins/AccountPlugin.scala
+++ src/main/scala/plugins/AccountPlugin.scala
@@ -1,5 +1,7 @@
 package net.node3.scalabot.plugins
 
+import scala.collection.immutable.Map
+
 import net.node3.scalabot.{ Plugin, MessageSource }
 import net.node3.scalabot.data._
 
@@ -9,36 +11,42 @@ class AccountPlugin extends Plugin {
 
   val rounds = 12
   val userRepository: UserRepository = new UserRepositoryImpl
+  val messages = Map[String, (MessageSource, String, String) => Option[String]](
+    "register" -> handleRegister,
+    "identify" -> handleIdentify
+  )
 
-  def apply(from: MessageSource, to: String, message: String) : Option[String] =
-    if(message.toLowerCase.startsWith("register")) {
-      for(ircName <- from.name; hostname <- from.hostname) yield
-        message.split(" ") match {
-          case Array(_, name, password, _*) =>
-            if(userRepository.getUser(name).isEmpty) {
-              userRepository.insertUser(name, password.bcrypt(rounds), s"$ircName@$hostname").map { u =>
-                s"${u.name} was registered"
-              } getOrElse(s"$name could not be registered")
-            } else s"$name could not be registered"
-          case _ => "Invalid format for the register command. The format is /msg botname register <name> <password>"
-        }
-    } else if(message.toLowerCase.startsWith("identify")) {
-      for(ircName <- from.name; hostname <- from.hostname) yield
-        message.split(" ") match {
-          case Array(_, name, password, _*) =>
-            userRepository.getUser(name).filter(u => password.isBcrypted(u.password)).map { user =>
-              val combinedHost = s"$ircName@$hostname"
-              userRepository.updateUser(user.userId, user.copy(hostname = combinedHost, lastIdentified = DateTime.now)).map { updatedUser =>
-                s"Successfully identified as $name. The account's hostname has been updated to $combinedHost."
-              }.getOrElse("Could not update user")
-            }.getOrElse("Invalid username/password combination")
-          case _ => "Invalid format for the identify command. The format is /msg botname identify <name> <password>"
-        }
-
-    } else {
-      None
+  def apply(from: MessageSource, to: String, message: String): Option[String] =
+    message.split(" ") match {
+      case Array(command, _*) => messages.get(command).map(_(from, to, message)).getOrElse(None)
+      case _ => None
     }
 
   def handlesMessage(from: MessageSource, to: String, message: String) =
-    message.toLowerCase.startsWith("register") || message.toLowerCase.startsWith("identify")
+    messages.keySet.exists(message.toLowerCase.startsWith(_))
+
+  def handleRegister(from: MessageSource, to: String, message: String): Option[String] =
+    for(ircName <- from.name; hostname <- from.hostname) yield
+      message.split(" ") match {
+        case Array(_, name, password, _*) =>
+          if(userRepository.getUser(name).isEmpty) {
+            userRepository.insertUser(name, password.bcrypt(rounds), s"$ircName@$hostname").map { u =>
+              s"${u.name} was registered"
+            } getOrElse(s"$name could not be registered")
+          } else s"$name could not be registered"
+        case _ => "Invalid format for the register command. The format is /msg botname register <name> <password>"
+      }
+
+  def handleIdentify(from: MessageSource, to: String, message: String): Option[String] =
+    for(ircName <- from.name; hostname <- from.hostname) yield
+      message.split(" ") match {
+        case Array(_, name, password, _*) =>
+          userRepository.getUser(name).filter(u => password.isBcrypted(u.password)).map { user =>
+            val combinedHost = s"$ircName@$hostname"
+            userRepository.updateUser(user.userId, user.copy(hostname = combinedHost, lastIdentified = DateTime.now)).map { updatedUser =>
+              s"Successfully identified as $name. The account's hostname has been updated to $combinedHost."
+            }.getOrElse("Could not update user")
+          }.getOrElse("Invalid username/password combination")
+        case _ => "Invalid format for the identify command. The format is /msg botname identify <name> <password>"
+      }
 }
