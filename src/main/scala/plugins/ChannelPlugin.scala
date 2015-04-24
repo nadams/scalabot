1f9e88969c75631677ac9e0a3e7258f2f7e7c387
Nick Adams <nick.adams@rakuten.com>
2015-04-24 07:41:38 -0600

    Ignoring messages from channels on join channel handler

--- src/main/scala/plugins/ChannelPlugin.scala
+++ src/main/scala/plugins/ChannelPlugin.scala
@@ -4,10 +4,12 @@ import akka.actor.ActorRef
 
 import net.node3.scalabot._
 import net.node3.scalabot.data._
+import net.node3.scalabot.config.Conf
 
 import Messages._
 
 class ChannelPlugin extends Plugin with PluginHelper {
+  val botName = Conf.nick
   val userRepository: UserRepository = new UserRepositoryImpl
   override val messages = Map[String, MessageHandler](
     "join" -> handleJoin
@@ -16,15 +18,17 @@ class ChannelPlugin extends Plugin with PluginHelper {
   def handleJoin(from: MessageSource, to: String, message: String, bot: ActorRef): Option[String] =
     message.split(" ") match {
       case Array(_, channelName, _*) =>
-        userRepository.getUser(from.source).map { user =>
-          for(name <- from.name; hostname <- from.hostname) yield
-            if(user.hostname == s"$name@$hostname" && PermissionFlags.isAdmin(user.permissions)) {
-              bot ! JoinChannelCommand(channelName)
-              s"Joining channel $channelName"
-            } else {
-              "Sorry, you are not authorized"
-            }
-        }.getOrElse(Some("Sorry, you are not authorized"))
+        if(to == botName) {
+          userRepository.getUser(from.source).map { user =>
+            for(name <- from.name; hostname <- from.hostname) yield
+              if(user.hostname == s"$name@$hostname" && PermissionFlags.isAdmin(user.permissions)) {
+                bot ! JoinChannelCommand(channelName)
+                s"Joining channel $channelName"
+              } else {
+                "Sorry, you are not authorized"
+              }
+          }.getOrElse(Some("Sorry, you are not authorized"))
+        } else None
       case _ => None
     }
 }
