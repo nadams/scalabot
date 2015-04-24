93fd9b9da370f5f9cdb5abe0fe2b74c20d775af9
Nick Adams <nick.adams@rakuten.com>
2015-04-24 09:25:54 -0600

    Filtering out empty messages from plugins

--- src/main/scala/BotCommandHandler.scala
+++ src/main/scala/BotCommandHandler.scala
@@ -3,7 +3,15 @@ package net.node3.scalabot
 import akka.actor.ActorRef
 
 class BotCommandHandler(val plugins: Seq[Plugin]) {
-  def handleMessage(from: MessageSource, to: String, message: String, bot: ActorRef): Seq[String] = plugins.flatMap(_(from, to, message, bot))
-  def handlesMessage(from: MessageSource, to: String, message: String): Boolean = plugins.exists(_.handlesMessage(from, to, message))
+  def handleMessage(from: MessageSource, to: String, message: String, bot: ActorRef): Seq[String] =
+    plugins.flatMap { plugin =>
+      plugin(from, to, message, bot).map { result =>
+        if(result == "") Some(result)
+        else None
+      }.getOrElse(None)
+    }
+
+  def handlesMessage(from: MessageSource, to: String, message: String): Boolean =
+    plugins.exists(_.handlesMessage(from, to, message))
 }
 
