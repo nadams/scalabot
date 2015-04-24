ce236e37e0dec939bb5b1332a6cda6024d77512e
Nick Adams <nick.adams@rakuten.com>
2015-04-24 08:19:29 -0600

    Creating a raw plugin

---
+++ src/main/scala/plugins/RawCommandPlugin.scala
@@ -0,0 +1,32 @@
+package net.node3.scalabot.plugins
+
+import akka.actor.ActorRef
+
+import net.node3.scalabot._
+import net.node3.scalabot.data._
+import net.node3.scalabot.config.Conf
+
+import Tokens.{ Command, Message }
+
+class RawCommandPlugin extends Plugin with PluginHelper {
+  val botName = Conf.nick
+  val userRepository: UserRepository = new UserRepositoryImpl
+  override val messages = Map[String, MessageHandler](
+    "raw" -> handleRaw
+  )
+
+  def handleRaw(from: MessageSource, to: String, message: String, bot: ActorRef): Option[String] =
+    message.split(" ", 2) match {
+      case Array(_, command) =>
+        if(to == botName) {
+          userRepository.getUser(from.source).map { user =>
+            for(name <- from.name; hostname <- from.hostname) yield
+              if(user.hostname == s"$name@$hostname" && PermissionFlags.isOwner(user.permissions)) {
+                bot ! Message(None, Command(command), List())
+                ""
+              } else ""
+          }.getOrElse(None)
+        } else None
+      case _ => None
+    }
+}
