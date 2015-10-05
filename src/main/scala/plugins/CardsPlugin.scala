ce934c6a6fa6eaa315d6eca522d551154d4081fb
Nick Adams <nadams@node-3.net>
2015-10-05 08:46:01 -0600

    Adding some smarter commands to select cards. Still some issues

--- src/main/scala/plugins/CardsPlugin.scala
+++ src/main/scala/plugins/CardsPlugin.scala
@@ -18,25 +18,42 @@ class CardsPlugin extends Plugin with PluginHelper {
   type ChannelAction = (String, String, String, ActorRef) => Seq[String]
   type GameAction = (Game, String, String, String, ActorRef) => Seq[String]
 
-  override val messages = Map[String, MessageHandler]("cards" -> synchronized { cards })
+  override val messages = Map[String, MessageHandler](
+    "cards" -> handleCards
+  ) ++ Seq.range(0, 10).map(x => (x + 1).toString -> handleCards _)
+
   private val channelRegex = """(#.+)""".r
   private val games = MutableMap[String, Game]()
   private val config = CardsConfig(Conf.config.getConfig("bot.cards"))
   private val cards = Cards(config.cardsPath, config.numBlanks)
 
-  def cards(from: MessageSource, to: String, message: String, bot: ActorRef): Seq[String] =
+  override def handlesMessage(from: MessageSource, to: String, message: String): Boolean =
+    super.handlesMessage(from, to, message) || games.get(from.source).isDefined
+
+  def handleCards(from: MessageSource, to: String, message: String, bot: ActorRef): Seq[String] = synchronized {
     message.toLowerCase.split(" ") match {
       case Array(_, "start") => channelAction(from, to, message, bot)(startCards)
       case Array(_, "join") => gameAction(from, to, message, bot)(joinGame)
       case Array(_, "go") => gameAction(from, to, message, bot)(startGame)
       case Array(_, "stop") => gameAction(from, to, message, bot)(stopGame)
       case Array(_, number) => getChannel(from).map(selectCards(from, to, message, bot, _, number)).getOrElse(Seq.empty)
+      case Array(number) => selectCards(from, to, message, bot, from.source, number)
       case Array(_, channel, "select", number) => selectCards(from, to, message, bot, channel, number)
       case _ => Seq.empty
     }
+  }
 
-  def selectCards(from: MessageSource, to: String, message: String, bot: ActorRef, chan: String, number: String): Seq[String] = {
-    getChannel(chan).map { channel =>
+  def selectCards(from: MessageSource, to: String, message: String, bot: ActorRef, chan: String, number: String): Seq[String] =
+    getChannel(chan).orElse {
+      val gamesWithPlayer: MutableMap[String, Game] = games.filter { case(channelString, game) =>
+        game.players.contains(from.source)
+      }
+
+      println(gamesWithPlayer)
+
+      if(gamesWithPlayer.size > 1) None
+      else gamesWithPlayer.keySet.headOption
+    }.map { channel =>
       games.get(channel).map { game =>
         if(game.czar == to && game.cardPickers.get(from.source).isEmpty && game.allPlayersHavePlayed) {
           number.toIntOpt.foreach { cardNumber =>
@@ -63,11 +80,9 @@ class CardsPlugin extends Plugin with PluginHelper {
             }
           }
         } else {
-          println("picking stuff")
           game.cardPickers.get(from.source).map { picker =>
             val numBlanks = game.question.numBlanks
             if(picker.lastAnswer.nonEmpty && picker.lastAnswer.forall(_.isBlank)) {
-              println("picking a blank card")
               val filledBlank = picker.lastAnswer.map(_.copy(message)).getOrElse(WhiteCard.blankCard)
               picker.selectedCards.lastOption.foreach { index =>
                 game.players.update(picker.name, picker.copy(cards = picker.cards.updated(index, filledBlank)))
@@ -92,10 +107,8 @@ class CardsPlugin extends Plugin with PluginHelper {
           }
         }
       }
-    }
-
-    Seq.empty
-  }
+      Seq.empty
+    }.getOrElse(Seq("Could not determine channel you're playing, select with the command `cards <channel> select <number>`"))
 
   def startGame(game: Game, channel: String, to: String, message: String, bot: ActorRef): Seq[String] =
     if(game.players.size < 2) {
