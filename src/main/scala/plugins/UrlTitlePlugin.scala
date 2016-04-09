0600c5efcfb82b0549eb05a8942e1fa6b986053d
Nick Adams <nadams@node-3.net>
2016-04-09 12:51:07 -0600

    Adding a throttle to url title

--- src/main/scala/plugins/UrlTitlePlugin.scala
+++ src/main/scala/plugins/UrlTitlePlugin.scala
@@ -12,6 +12,7 @@ import com.typesafe.scalalogging.LazyLogging
 import dispatch._
 import dispatch.Defaults._
 import net.ruippeixotog.scalascraper.browser.JsoupBrowser
+import org.joda.time.DateTime
 
 import net.node3.scalabot._
 import net.node3.scalabot.data._
@@ -20,30 +21,38 @@ class UrlTitlePlugin extends Plugin with LazyLogging {
   import net.ruippeixotog.scalascraper.dsl.DSL._
   import net.ruippeixotog.scalascraper.dsl.DSL.Extract._
   import net.ruippeixotog.scalascraper.dsl.DSL.Parse._
+  import net.ruippeixotog.scalascraper.util.Validated._
 
   val timeout = 15.seconds
+  val throttle = 2
 
   private val urlRegex = """(https?:\/\/(?:www\.|(?!www))[^\s\.]+\.[^\s]{2,}|www\.[^\s]+\.[^\s]{2,})""".r
   private val browser = JsoupBrowser()
 
+  private var lastTitle = DateTime.now
+
   override def handlesMessage(from: MessageSource, to: String, message: String): Boolean = true
 
   def apply(from: MessageSource, to: String, message: String, bot: ActorRef): Seq[String] =
-    urlRegex.findFirstIn(message).map { m =>
-      val response = Http.configure(_.setFollowRedirects(true))(url(m) > as.Response(getTitle)).recover {
-        case e: TimeoutException => Some(s"Timeout after ${timeout.toString}")
-        case e: Throwable => {
-          logger.error("Could not get url title", e)
+    if(lastTitle.plusSeconds(throttle).isBeforeNow)
+      urlRegex.findFirstIn(message).map { m =>
+        val response = Http.configure(_.setFollowRedirects(true))(url(m) > as.Response(getTitle)).recover {
+          case e: TimeoutException => Some(s"Timeout after ${timeout.toString}")
+          case e: Throwable => {
+            logger.error("Could not get url title", e)
 
-          Some(s"Could not get URL title, try again later...")
+            Some(s"Could not get URL title, try again later...")
+          }
         }
-      }
 
-      val title = Await.result(response, timeout)
+        Await.result(response, timeout).map { x =>
+          lastTitle = DateTime.now
 
-      Seq(s"$to: URL Title for $m - $title")
-    }.getOrElse(Seq.empty)
+          Seq(s"$to: URL Title for $m - $x")
+        }.getOrElse(Seq.empty)
+      }.getOrElse(Seq.empty)
+    else Seq.empty
 
-  def getTitle(r: Response): String =
-    browser.parseString(r.getResponseBody).title
+  def getTitle(r: Response): Option[String] =
+    (browser.parseString(r.getResponseBody) >?> element("title")).map(x => Some(x.text.trim)).getOrElse(None)
 }
