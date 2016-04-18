9cb2aab35d5b9471ee6f043d50b419f9f2e7311c
Nick Adams <nadams@node-3.net>
2016-04-18 16:32:39 -0600

    Adding magnet link title detection

--- src/main/scala/plugins/UrlTitlePlugin.scala
+++ src/main/scala/plugins/UrlTitlePlugin.scala
@@ -13,6 +13,7 @@ import dispatch._
 import dispatch.Defaults._
 import net.ruippeixotog.scalascraper.browser.JsoupBrowser
 import org.joda.time.DateTime
+import org.apache.commons.codec.net.URLCodec
 
 import net.node3.scalabot._
 import net.node3.scalabot.data._
@@ -30,7 +31,9 @@ class UrlTitlePlugin extends Plugin with LazyLogging {
   val throttle = 2
 
   private val urlRegex = """(https?:\/\/(?:www\.|(?!www))[^\s\.]+\.[^\s]{2,}|www\.[^\s]+\.[^\s]{2,})""".r
+  private val magnetRegex = """(magnet:.+)""".r
   private val browser = JsoupBrowser()
+  private val urlCodec = new URLCodec()
   private def client = Http.configure(_.setFollowRedirects(true))
   private val defaultRecover: PartialFunction[Throwable, Option[String]] = {
     case e: TimeoutException => Some(s"Timeout after ${timeout.toString}")
@@ -47,20 +50,37 @@ class UrlTitlePlugin extends Plugin with LazyLogging {
 
   def apply(from: MessageSource, to: String, message: String, bot: ActorRef): Seq[String] =
     if(lastTitle.plusSeconds(throttle).isBeforeNow)
-      urlRegex.findFirstIn(message).flatMap { m =>
-        val isValidRequest = Await.result(client(url(m).HEAD > as.Response(isHtml)).recover {
-          case e: Throwable => Map("failed" -> false)
-        }, timeout).forall(x => x._2)
+      message match {
+        case urlRegex(m) => urlTitle(to, m)
+        case magnetRegex(m) => magnetTitle(m)
+        case _ => Seq.empty
+      }
+    else Seq.empty
 
-        if(isValidRequest)
-          Await.result(client(url(m) > as.Response(getTitle)).recover(defaultRecover), timeout).map { x =>
-            lastTitle = DateTime.now
+  private def magnetTitle(m: String): Seq[String] = {
+    m.replaceFirst("""^magnet:\?""", "")
+      .split("&")
+      .map(_.split("="))
+      .filter(_.length >= 2)
+      .map(x => x(0) -> x(1)).toMap
+      .get("dn")
+      .map(x => Seq(s"Magnet Link Title: ${urlCodec.decode(x)}"))
+      .getOrElse(Seq.empty)
+  }
 
-            Some(Seq(s"$to: URL Title for $m - $x"))
-          }.getOrElse(None)
-        else None
+  private def urlTitle(to: String, m: String): Seq[String] = {
+    val isValidRequest = Await.result(client(url(m).HEAD > as.Response(isHtml)).recover {
+      case e: Throwable => Map("failed" -> false)
+    }, timeout).forall(x => x._2)
+
+    if(isValidRequest)
+      Await.result(client(url(m) > as.Response(getTitle)).recover(defaultRecover), timeout).map { x =>
+        lastTitle = DateTime.now
+
+        Seq(s"$to: URL Title for $m - $x")
       }.getOrElse(Seq.empty)
     else Seq.empty
+  }
 
   private def isHtml(r: Response): Validations = Map(
     "content-type" -> r.getContentType.toLowerCase.startsWith("text/html"),
