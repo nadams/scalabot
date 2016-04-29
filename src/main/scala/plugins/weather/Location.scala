e06bc03b8d4a1539b234990425fbf98e3e048d31
Nick Adams <nadams@node-3.net>
2016-04-29 09:00:20 -0600

    Making things optional

--- src/main/scala/plugins/weather/Location.scala
+++ src/main/scala/plugins/weather/Location.scala
@@ -1,8 +1,8 @@
 package net.node3.scalabot.plugins.weather
 
 case class Location(
-  val `type`: Int,
-  val id: Int,
+  val `type`: Option[Int],
+  val id: Option[Int],
   val message: Double,
   val country: String,
   val sunrise: Long,
@@ -10,10 +10,10 @@ case class Location(
 )
 
 case class ForecastLocation(
-  val id: Int,
+  val id: Option[Int],
   val name: String,
   val coord: Coordinate,
   val country: String,
-  val population: Int
+  val population: Option[Int]
 )
 
